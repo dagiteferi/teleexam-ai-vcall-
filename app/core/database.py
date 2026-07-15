20dde15b10a3ec36442804fd183d1aba81bd08d1
Lidiya Mergiya <mergiyalidiya@gmail.com>
2026-07-15 11:37:53 +0300

    chore: add SQLAlchemy table models

---
+++ app/core/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase
+
+from app.core.config import settings
+
+
+engine = create_async_engine(
+    settings.sqlalchemy_database_url,
+    echo=False,
+)
+
+async_session = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+class Base(DeclarativeBase):
+    pass
